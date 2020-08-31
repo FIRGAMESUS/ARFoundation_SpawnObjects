365971edc7945960977c2e97a637ec44154b6944
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-31 17:39:17 +0300

    ARFoundation_SpawnObjects (04.01.20)

---
+++ Assets/Test/ObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator;
+    private bool isObject = false;
+    private GameObject obj;
+    private float speed = 10f;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update()
+    {
+        if(!isObject && (Input.touchCount > 0) && (Input.touches[0].phase == TouchPhase.Began))
+        {
+            obj = Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            isObject = true;
+        }
+        else
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position, placementIndicator.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
